c2d194bcd9cabc666eb1e1e12b9b98ee0f1ca50c
Sai-Kolla85 <2300031410@kluniversity.in>
2025-11-16 20:49:21 +0530

    Change BrowserRouter basename from '/HMS/' to '/EMS/'

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/HMS/'>
+    <BrowserRouter basename='/EMS/'>
       <AuthProvider>
         <App />
       </AuthProvider>
